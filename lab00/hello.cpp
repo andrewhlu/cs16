8b207b19a9a24922fb91f963cd55df8834113c05
andrewhlu <alu@ucsb.edu>
2018-04-05 23:59:54 -0700

    Upload lab00

---
+++ lab00/hello.cpp
@@ -0,0 +1,11 @@
+//hello.cpp
+//This is a program to explore simple input output in C++
+
+#include <iostream> //include the modules for output and input
+using namespace std; //using functions in the standard library
+
+int main() { //Execution always starts here
+	cout << "Hello, world!" << endl;
+	cout << "I am ready for CS16!" << endl;
+	return 0;
+}
