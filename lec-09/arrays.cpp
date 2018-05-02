891e4d9abe4866556b94028d0871cce9295880e0
andrewhlu <alu@ucsb.edu>
2018-05-02 14:34:53 -0700

    Pointers introduction

---
+++ lec-09/arrays.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+double averageScore(double sc[], int length);
+
+int main() {
+	int* p;
+	int y = 3;
+
+	p = &y;
+
+	cout << "Value of p is: " << p << endl;
+	cout << "Value of *p is: " << *p << endl;
+	cout << "Value of y is: " << y << endl;
+}
