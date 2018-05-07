29d590cfdb13069fadaef1d9248cc9145e32a220
andrewhlu <alu@ucsb.edu>
2018-05-07 14:48:52 -0700

    Pass structs by value

--- lec-10/structs.cpp
+++ lec-10/structs.cpp
@@ -12,12 +12,18 @@ struct Box {
 	double height;
 };
 
+void printBox(Box b){
+	cout << "(" << b.ul.x << "," << b.ul.y << ")" << endl;
+	cout << "Width: " << b.width << endl;
+	cout << "Height: " << b.height << endl;
+}
+
 int main() {
 	Box b1 = {{500,600},10,20};
+	b1.ul.x = 50;
 
 	cout << "Created a box" << endl;
-	cout << "(" << b1.ul.x << "," << b1.ul.y << ")" << endl;
-	cout << "Width: " << b1.width << endl;
-	cout << "Height: " << b1.height << endl;
+	printBox(b1);
+
 	return 0;
 }
