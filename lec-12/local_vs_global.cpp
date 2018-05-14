8b83a64da2337294cd750f84a7586e742c651d1d
andrewhlu <alu@ucsb.edu>
2018-05-14 15:08:04 -0700

    Access an array on the heap

--- lec-12/local_vs_global.cpp
+++ lec-12/local_vs_global.cpp
@@ -8,7 +8,24 @@ int* createAnInt() {
 	return p;
 }
 
+int* createAnArrayOfInts(int len) {
+	int* p = new int[len];
+
+	for(int i = 0; i < len; i++) {
+		p[i] = 10; //Pointer arithmetic works here, equivalent to *(p+i)
+	}
+
+	return p;
+}
+
 int main() {
 	int *q = createAnInt();
 	cout << *q << endl;
+
+	q = createAnArrayOfInts(5);
+	cout << "Accessing an array on the heap" << endl;
+	for(int i = 0; i < 5; i++) {
+		cout << q[i] << " ";
+	}
+	cout << endl;
 }
