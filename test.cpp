76ba458cf4919c39d37163e484e596b7641e660f
andrewhlu <alu@ucsb.edu>
2018-05-15 22:11:25 -0700

    Test address of arrays

---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int arr[] = {10, 20, 40, 60};
+	int *p = arr;
+
+	cout << arr << endl;
+	cout << arr+1 << endl;
+	cout << arr+2 << endl;
+	cout << arr+3 << endl;
+
+	// cout << *p << endl;
+	// cout << *(p+2) << endl;
+	// cout << p[3] << endl;
+	// cout << &p << endl;
+
+	// cout << p << endl;
+}
