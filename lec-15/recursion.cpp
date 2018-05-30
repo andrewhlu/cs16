09fb6ec5f5f7f7f0822ccd1b83c583b3762aa60a
andrewhlu <alu@ucsb.edu>
2018-05-30 14:50:19 -0700

    Count odd recursive

--- lec-15/recursion.cpp
+++ lec-15/recursion.cpp
@@ -11,12 +11,28 @@ void printArray(int arr[], int len) {
 	}
 }
 
+int countOddRecursive(int a[], int len) {
+	if(len == 0) { //Base Case
+		return 0;
+	}
+	else if(a[0] % 2 == 1) {
+		//Current element is odd
+		return 1 + countOddRecursive(a+1, len-1);
+	}
+	else {
+		//Current element is even
+		return countOddRecursive(a+1, len-1);
+	}
+}
+
 void funWithArrays() {
-	int arr[] = {5,3,2,1,345,46,66,6,5};
+	int arr[] = {5,3,2,1,345,46,66,6,5,13,10};
 	int len = sizeof(arr)/sizeof(int);
 
 	printArray(arr, len);
 	cout << endl;
+	
+	cout << "# Odd (R) " << countOddRecursive(arr, len) << endl;
 }
 
 int main() {
