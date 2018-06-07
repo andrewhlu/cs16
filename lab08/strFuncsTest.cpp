225cf76954c112273337e09ad72247bf6786f2c9
andrewhlu <alu@ucsb.edu>
2018-06-07 00:40:33 -0700

    Test cases for isPalindrome

--- lab08/strFuncsTest.cpp
+++ lab08/strFuncsTest.cpp
@@ -22,28 +22,41 @@ int main(int argc, char* argv[]) {
 
 			string s1 = "UCSB";
 			string s2 = "cubs";
-
 			ASSERT_TRUE(isAnagram(s1, s2));
 
 			string s3 = "qwertyuiopasdfghjklzxcvbnm";
 			string s4 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
 			ASSERT_TRUE(isAnagram(s3, s4));
 
 			string s5 = "andrewlu";
 			string s6 = "Lu, Andrew";
-
 			ASSERT_TRUE(isAnagram(s5, s6));
 
 			string s7 = "yes";
 			string s8 = "no";
-
 			ASSERT_FALSE(isAnagram(s7, s8));
 
 			string s9 = "I love to code";
 			string s10 = "I love to debug";
-
 			ASSERT_FALSE(isAnagram(s9, s10));
 		}
+	else if(testNum == 2) {
+		START_TEST_GROUP("IS_PALINDROME");
+
+		string s1 = "racedcar";
+		ASSERT_FALSE(isPalindrome(s1));
+
+		string s2 = "Hannah";
+		ASSERT_TRUE(isPalindrome(s2));
+
+		string s3 = "ASDFGHJKLlkjhgfdsa";
+		ASSERT_TRUE(isPalindrome(s3));
+
+		string s4 = "Tacocat";
+		ASSERT_TRUE(isPalindrome(s4));
+
+		string s5 = "Andrea";
+		ASSERT_FALSE(isPalindrome(s5));
 	}
+}
 }
